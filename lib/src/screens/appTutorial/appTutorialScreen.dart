a49f5ad4be78fa05a9c272e77062f50680fff256
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2023-10-23 16:12:10 -0500

    tutorial finished

--- lib/src/screens/appTutorial/appTutorialScreen.dart
+++ lib/src/screens/appTutorial/appTutorialScreen.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: file_names
-import 'dart:ffi';
-
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
